38f71477762b68591f0aff7643263ac7904b0db8
Mark <mark28ten@gmail.com>
2021-10-13 12:44:08 -0700

    finished ll CC

--- js401/linked-list/__tests__/linkedList.test.js
+++ js401/linked-list/__tests__/linkedList.test.js
@@ -24,6 +24,49 @@ describe('Linked list', () => {
     expect(list.head.next.value).toEqual(newValue);
   });
 
-  
+  it('should insert a new node to the head', () => {
+    let list = new LinkedList();
+    let initialValue = 'Oreo';
+    list.insert(initialValue);
+    // console.log('Inside insert:', list);
+    expect(list.head.value).toEqual(initialValue);
+
+    let newValue = 'Gibby';
+    list.insert(newValue);
+    // console.log('Inside new list:', list);
+    expect(list.head.value).toEqual(newValue);
+  });
+
+  it('should check to see if node exist', () => {
+    let list = new LinkedList();
+    let initialValue = 'Oreo';
+    list.insert(initialValue);
+
+    let newValue = 'Octane';
+    list.append(newValue);
+
+    // console.log('inside made list', list);
+
+    let falseVal = 'Gibby';
+
+    // console.log('inside boolean: should be false:', list.includes(falseVal));
+    expect(list.includes(falseVal)).toBeFalsy();
+    // console.log('inside boolean should be true:', list.includes(newValue));
+    expect(list.includes(newValue)).toBeTruthy();
+  });
+
+  it('should return the values into a string', () => {
+    let list = new LinkedList();
+    let initialValue = 'Oreo';
+    list.insert(initialValue);
+    let secondValue = 'Octane';
+    list.append(secondValue);
+    // console.log('inside string', list); 
+    // list.toString();
+    let stringTest = list.toString();
+    // console.log('toString test output:', stringTest);
+
+    expect.stringContaining(stringTest);
+  });
 
 });
--- js401/linked-list/lib/linkedList.js
+++ js401/linked-list/lib/linkedList.js
@@ -13,18 +13,62 @@ class LinkedList {
     let node = new Node(value);
 
     // If list is empty
-    if(!this.head) {
+    if (!this.head) {
       this.head = node;
       return this;
     }
     // If nodes exist
     let currentNode = this.head;
-    while(currentNode.next) {
+    while (currentNode.next) {
       currentNode = currentNode.next;
     }
     currentNode.next = node;
     return this;
   }
+
+  // Insert to the head
+  insert(value) {
+    let node = new Node(value);
+
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+
+    node.next = this.head;
+    this.head = node;
+    return this;
+  }
+
+  includes(value) {
+    // Not sure if this is needed but works w/o it
+    // let node = new Node(value);
+
+    let currentNode = this.head;
+    while (currentNode != null) {
+      if (currentNode.value === value) {
+        return true;
+      }
+    // Updates the current node?
+    // currentNode.next = node;
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
+
+  toString() {
+    // Still not sure if I need this but works w/o it
+    // let node = new Node();
+
+    let currentNode = this.head;
+    let string = "";
+    while (currentNode) {
+      string += `{${currentNode.value}}` + " -> ";
+      currentNode = currentNode.next;
+    }
+    return `"${string}NULL"`;
+  }
+
 }
 
 module.exports = LinkedList;
