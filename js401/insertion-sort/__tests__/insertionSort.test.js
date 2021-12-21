93eb13e05ff8f86ec92c78c8c29fc8cdbb5cea49
Mark <mark28ten@gmail.com>
2021-12-21 14:04:22 -0800

    finished most of the CC26

--- js401/insertion-sort/__tests__/insertionSort.test.js
+++ js401/insertion-sort/__tests__/insertionSort.test.js
@@ -1,2 +1,22 @@
 'use strict';
 
+const insertionSort = require('../insertionSort');
+
+let testArr = [8, 4, 23, 42, 16, 15];
+let correctOrder = [4, 8, 15, 16, 23, 42];
+let failTest = [8, 4, 23, 42, 16, 15]
+
+describe('Insertion Sort Function', () => {
+  
+  it('should correctly sort the array', () => {
+    let test = insertionSort(testArr);
+    expect(test).toBeDefined();
+    expect(test).toEqual(correctOrder);
+  });
+
+  it('should not equal the same array', () => {
+    let test = insertionSort(testArr);
+    expect(test).not.toBe(failTest);
+  })
+
+})
