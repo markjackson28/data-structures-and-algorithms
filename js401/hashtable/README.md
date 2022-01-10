c38d24a76c46aaef68c9037d3329dd90ae1a5043
Mark <mark28ten@gmail.com>
2022-01-10 15:44:07 -0800

    finished hashTable

--- js401/hashtable/README.md
+++ js401/hashtable/README.md
@@ -1,11 +1,30 @@
 # Hashtables
-<!-- Short summary or background information -->
+
+'Hash Tables are a data structure that allow you to create a list of paired values. You can then retrieve a certain value by using the key for that value, which you put into the table beforehand.'
 
 ## Challenge
-<!-- Description of the challenge -->
 
-## Approach & Efficiency
-<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Implement a Hashtable Class with the following methods:
 
-## API
-<!-- Description of each method publicly available in each of your hashtable -->
+- add
+  - Arguments: key, value
+  - Returns: nothing
+  - This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
+- get
+  - Arguments: key
+  - Returns: Value associated with that key in the table
+- contains
+  - Arguments: key
+  - Returns: Boolean, indicating if the key exists in the table already.
+- hash
+  - Arguments: key
+  - Returns: Index in the collection for that key
+
+## Efficiency
+
+Space: O(n)  
+Search: O(1)
+
+## Resources
+- [JavaScript Hash Table – Associative Array Hashing in JS](https://www.freecodecamp.org/news/javascript-hash-table-associative-array-hashing-in-js/)
+- [Data Structures: Hash Tables](https://www.youtube.com/watch?v=shs0KM3wKv8)
---
+++ js401/hashtable/hashtable.js
@@ -0,0 +1,90 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  add(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = node;
+  }
+  values() {
+    let values = [];
+    let current = this.head;
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+    return values;
+  }
+}
+
+class Hashtable {
+  constructor(size) {
+    this.size = size;
+    this.map = new Array(size);
+  }
+  add(key, value) {
+    // calculate the has of key 
+    let hash = this.hash(key);
+    // console.log('hash', hash)
+    // check if there is a value
+    if (!this.map[hash]) { this.map[hash] = new LinkedList(); }
+    // assume there is a LL
+    // create my entry
+    let entry = { [key]: value } // Zork:3 -> "zork":3 // value of Node
+    // add to LL
+    this.map[hash].add(entry);
+  }
+  get(key) {
+    let hash = this.hash(key);
+    if(this.map[hash] === undefined) {
+      return 'Key does not exist';
+    }
+    return this.map[hash];
+   }
+  contains(key) {
+    let hash = this.hash(key);
+    if (this.map[hash]) return true;
+    else return false;
+  }
+  hash(key) {
+    // with the key, find every ASCII value for each i in the string
+    return key.split('').reduce((prev, curr) => {
+      return prev + curr.charCodeAt(0)
+    }, 0) * 599 % this.size;
+
+    // add up all the ascii values
+    // multiply by a prime number 
+    // modulo against the size of your hashmap
+    // return the index
+  }
+}
+
+// let myhash = new Hashtable(20);
+// myhash.add('Octane', 'Speed Runner');
+// myhash.add('Gibby', 'On demand cover');
+// myhash.add('Valk', 'Jetpack');
+// myhash.add('Bloodhound', 'Scanner');
+
+// let getHash = myhash.contains('Octane');
+
+// console.log(getHash);
+
+module.exports = Hashtable;
