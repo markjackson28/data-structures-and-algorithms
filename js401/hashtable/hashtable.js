9f94c002c253cca8ef801028b4a5bca5c74512b8
Mark <mark28ten@gmail.com>
2022-01-13 20:49:39 -0800

    just added treeIntersection folder

--- js401/hashtable/hashtable.js
+++ js401/hashtable/hashtable.js
@@ -75,39 +75,16 @@ class Hashtable {
     // modulo against the size of your hashmap
     // return the index
   }
-  repeatedTerm(string) {
-      let stringSplit = string.toLowerCase().split(" ");
-      // hashset for storing words
-      let hashSet = new Hashtable(stringSplit.length);
-      // console.log(hashSet)
-
-      // store the words of string in hashset
-      for (let i = 0; i < stringSplit.length; i++) {
-        // console.log(i)
-        // if word exists return
-        if (hashSet.contains(stringSplit[i])) {
-          return stringSplit[i];
-        }
-        // insert new word to set
-        hashSet.add(stringSplit[i]);
-      }
-      return 'No Repetition';
-  }
 }
 
 let myhash = new Hashtable(100);
+// console.log(myhash);
 // myhash.add('Octane', 'Speed Runner');
 // myhash.add('Gibby', 'On demand cover');
 // myhash.add('Valk', 'Jetpack');
 // myhash.add('Bloodhound', 'Scanner');
 
 // let getHash = myhash.contains('Octane');
-let testString = 'Once upon a time, there was a brave princess who...';
-let testString2 = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way - in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...'
-let testString3 = 'It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn\'t know what I was doing in New York...';
-console.log(myhash.repeatedTerm(testString))
-console.log(myhash.repeatedTerm(testString2));
-console.log(myhash.repeatedTerm(testString3));
 
 // console.log(testString.toLowerCase());
 
